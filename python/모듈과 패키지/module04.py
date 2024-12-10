a5f702e48a8126f046c11ef35fcf59524aff8606
hhhhhhyun <duhrsiam93@naver.com>
2024-12-10 16:30:01 +0900

    fifth

--- python/모듈과 패키지/module04.py
+++ python/모듈과 패키지/module04.py
@@ -1,4 +1,5 @@
 #실습. 로또 번호 뽑기
+# my
 import random
 
 num = []
@@ -12,3 +13,27 @@ for i in range(6):
     
 num.sort()
 print(num)
+#방법1
+lotto = []
+while len(lotto) < 6 :
+    num = random.randint(1,45)
+    if num in lotto :
+        continue
+    lotto.append(num)
+    
+print(sorted(lotto))
+
+#방법2
+#set(집합)으로 구현하기
+lotto=set() 
+while True:
+    x= random.randint(1,45)
+    lotto.add(x)
+    if len(lotto) == 6:
+        break
+sorted_lotto = sorted(lotto)
+print(sorted_lotto) #set은 sort() dksehla, sorted() 이용해서 새로운 리스트로
+
+#방법3
+lotto = random.sample(range(1,46), 6)
+print(sorted(lotto))
