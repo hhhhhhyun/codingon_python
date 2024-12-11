450fc6c24ca17d472608f4ebab97efbf0d24fe7b
hhhhhhyun <duhrsiam93@naver.com>
2024-12-11 13:56:16 +0900

    eighth

--- python/9.파일/file.py
+++ python/9.파일/file.py
@@ -1,7 +1,14 @@
-f = open("./text.txt", "a") #"w" : 처음부터 새로 쓴다.
-f.write('Hello World!!!!addadd \n') #f.write의 반환값은 문자 길이
-f.close()
+#파일 입출력
+f = open("text.txt", "a") #"w" : 처음부터 새로 쓴다.
+f.write('Hello World!!!!addaddff\n') #f.write의 반환값은 문자 길이
+f.close() #꼭 닫아줘야함!, close하기 전엔 저장이 안됨
 
-f2 = open("./text.txt")
+f2 = open("text.txt")
 print(f2.read(2))
 f2.close()
+
+f3 = open("text.txt")
+print(f3.readline(), end="")
+print(f3.readline(), end="")
+print(f3.readline(), end="")
+f3.close()
--- python/9.파일/file2.py
+++
@@ -1,3 +0,0 @@
-f2 = open("text.txt")
-print(f2.read())
-f2.close()
